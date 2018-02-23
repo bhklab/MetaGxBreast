cb98134568f7cbf8011e606cdc08eaa5a86c1793
mzon7 <michaelzon7@gmail.com>
2018-02-22 21:35:02 -0500

    Add files via upload

--- tests/testthat/test_loadBreastEsets.R
+++ tests/testthat/test_loadBreastEsets.R
@@ -1,1 +1,15 @@
-#
+
+library(MetaGxBreast)
+
+context("Checking loadBreastEsets")
+
+
+test_that("ensure datasets and duplicates are properly loaded from the hub and package", {
+  esetsAndDuplicates = MetaGxBreast::loadBreastEsets()
+  esets = esetsAndDuplicates$esets
+  duplicates = esetsAndDuplicates$duplicates
+  expect_equal(duplicates[[1]], "DFHCC2.DFHCC2_REF12rep")
+  expect_equal(class(esets[[1]])[1], "ExpressionSet")
+  expect_equal(class(esets[[1]]@assayData$exprs), "matrix")
+  
+})
