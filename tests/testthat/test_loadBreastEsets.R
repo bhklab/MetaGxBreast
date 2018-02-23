15d53e62affa7af6fb251ddc44ff7160431e8305
mzon7 <michaelzon7@gmail.com>
2018-02-23 11:41:45 -0500

    Update test_loadBreastEsets.R

--- tests/testthat/test_loadBreastEsets.R
+++ tests/testthat/test_loadBreastEsets.R
@@ -5,7 +5,7 @@ context("Checking loadBreastEsets")
 
 
 test_that("ensure datasets and duplicates are properly loaded from the hub and package", {
-  esetsAndDuplicates = MetaGxBreast::loadBreastEsets()
+  esetsAndDuplicates = MetaGxBreast::loadBreastEsets(loadString = c("CAL", "DFHCC", "DFHCC2", "DFHCC3", "DUKE", "DUKE2", "EMC2"))
   esets = esetsAndDuplicates$esets
   duplicates = esetsAndDuplicates$duplicates
   expect_equal(duplicates[[1]], "DFHCC2.DFHCC2_REF12rep")
