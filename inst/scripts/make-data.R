e669f5dc8c4b6d07ad9c01694588133e07442c9c
mzon7 <michaelzon7@gmail.com>
2018-02-22 21:32:47 -0500

    Add files via upload

---
+++ inst/scripts/make-data.R
@@ -0,0 +1,5 @@
+
+#No processing has been applied to the gene expression data for the datasets. They have been left in the form they were obtained.
+#In the case of the GEO files the expression data would generally be in the form obtained after converting the results
+#from the microarray platform to gene expression values. In the NCI dataset that was taken from the supplementary files of the paper,
+#the dataset would be processed in whatever manner the authors of the paper described.
